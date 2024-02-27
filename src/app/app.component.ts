7520f812eb522270136f01b8b0f34018a5f73d00
Giorgio Borgo <giorgio.borgo@gmail.com>
2024-02-27 16:17:11 +0100

    ngx-translate funzionante

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,12 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'QTBWeb';
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en-US', 'it-IT']);
+    translate.setDefaultLang('it-IT');
+    translate.use('it-IT');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,25 +1,22 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { routes } from './app.routes';
+
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-import {
-  HttpClient,
-  provideHttpClient,
-  withFetch,
-} from '@angular/common/http';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
 }
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()),
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideHttpClient(withFetch()),
     importProvidersFrom(
       TranslateModule.forRoot({
         loader: {
